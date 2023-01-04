d8b65a729292d813901f91fb1b322fe41414667f
gomez-fernando <ferdgomez@hotmail.es>
2023-01-04 23:05:26 +0000

    Add models and seed

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,21 @@
 import Head from 'next/head'
 import Image from 'next/image'
-import { Inter } from '@next/font/google'
+import { PrismaClient } from '@prisma/client'
 
-const inter = Inter({ subsets: ['latin'] })
 
-export default function Home() {
+export default function Home({categories}) {
+  console.log(categories)
   return (
     <h1>App</h1>
   )
+}
+
+export const getServerSideProps = async () => {
+  const prisma = new PrismaClient();
+
+  const categories = await prisma.category.findMany();
+
+  return {props: {
+    categories
+  }}
 }
---
+++ prisma/data/categories.ts
@@ -0,0 +1,31 @@
+
+const categories = [
+  {
+      icon: "coffe",
+      name: "Café"
+    },
+    {
+      icon: "burger",
+      name: "Hamburguesas"
+    },
+    {
+      icon: "pizza",
+      name: "Pizzas"
+    },
+    {
+      icon: "donut",
+      name: "Donuts"
+    },
+    {
+      icon: "cake",
+      name: "Tartas"
+    },
+    {
+      icon: "cookie",
+      name: "Galletas"
+    }
+]
+
+export {
+  categories 
+}
